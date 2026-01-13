5487133cc4a67ba008575780dcc3d497a4007594
Ömer Apaydın <omerapaydin90@gmail.com>
2026-01-14 02:30:41 +0300

    Initial Commit

---
+++ SwiftAnimasyonTests/SwiftAnimasyonTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAnimasyonTests.swift
+//  SwiftAnimasyonTests
+//
+//  Created by Ömer on 14.01.2026.
+//
+
+import Testing
+@testable import SwiftAnimasyon
+
+struct SwiftAnimasyonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
