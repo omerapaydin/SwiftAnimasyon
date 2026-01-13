fb55667b47ccaabf152ecc4984c51d3fd6d26b53
Ömer Apaydın <omerapaydin90@gmail.com>
2026-01-14 02:50:51 +0300

    alpha

--- SwiftAnimasyon/ViewController.swift
+++ SwiftAnimasyon/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var yazi: UILabel!
+    @IBOutlet weak var resim: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func basla(_ sender: Any) {
+        alphaAnimasyonu()
+    }
+    
+    func alphaAnimasyonu(){
+        self.resim.alpha = 0
+        
+        UIView.animate(withDuration: 5) {
+            self.resim.alpha = 1
+        }
+        
+    }
+    
+    
 }
 
