cae32addf68586fa30bb7048ed26a3a2abc991f1
Ömer Apaydın <omerapaydin90@gmail.com>
2026-01-14 19:22:26 +0300

    ardisikAnimasyon

--- SwiftAnimasyon/ViewController.swift
+++ SwiftAnimasyon/ViewController.swift
@@ -21,7 +21,13 @@ class ViewController: UIViewController {
     @IBAction func basla(_ sender: Any) {
         //alphaAnimasyonu()
         //scaleAnimasyonu()
-        rotationAnimasyonu()
+        //rotationAnimasyonu()
+        //translationAnimasyonu()
+        //gecikmeAnimasyonu()
+        //ivmeAnimasyonu()
+        //tekrarAnimasyonu()
+        //cokluAnimasyonu()
+        ardisikAnimasyon()
     }
     
     func alphaAnimasyonu(){
@@ -46,6 +52,62 @@ class ViewController: UIViewController {
         }
     }
     
+    func translationAnimasyonu(){
+        UIView.animate(withDuration: 1) {
+            self.yazi.transform = CGAffineTransform(translationX: -50, y:0)
+        }
+    }
+    
+    func gecikmeAnimasyonu(){
+        UIView.animate(withDuration: 2, delay: 1) {
+            self.yazi.transform = CGAffineTransform(translationX: 0, y:100)
+        }
+    }
+    
+    
+    func ivmeAnimasyonu(){
+        UIView.animate(withDuration: 2, delay: 0 , options: .curveEaseIn) {
+            self.yazi.transform = CGAffineTransform(translationX: 0, y:100)
+        }
+    }
+    
+    func tekrarAnimasyonu(){
+        UIView.animate(withDuration: 1, delay: 0 , options: .repeat .union(.autoreverse)) {
+            self.resim.transform = CGAffineTransform(translationX: 0, y:100)
+        }
+    }
+    
+    func cokluAnimasyonu(){
+        UIView.animate(withDuration: 1) {
+            
+            let hareket = CGAffineTransform(translationX: 0, y: 100)
+            let kucul = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            let don = CGAffineTransform(rotationAngle: 90 * .pi/100)
+            
+            let x = hareket.concatenating(kucul)
+            
+            self.yazi.transform = don.concatenating(x)
+            self.yazi.alpha = 0
+             
+        }
+    }
+    
+    func ardisikAnimasyon(){
+        UIView.animate(withDuration: 1, animations: {
+            self.resim.alpha = 0
+        },completion: {
+            (true) in
+            UIView.animate(withDuration: 1, animations: {
+                self.yazi.alpha = 0
+            },completion: { (true) in
+            
+                UIView.animate(withDuration: 1, animations: {
+                    self.button.alpha = 0
+                },completion: nil)
+            })
+        })
+    }
+
     
 }
 
